ca409c68ea60da3090c02bc7bdc30e259089cb45
Marco Mazzai <mazzai.marco@gmail.com>
2023-10-28 15:25:22 +0200

    ( app ): add test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
-import Image from 'next/image'
+"use client";
+import React, { useEffect } from 'react';
 import styles from './page.module.css'
 
 export default function Home() {
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const backgroundColor = params.get('backgroundColor');
+
+    document.body.style.backgroundColor = backgroundColor || 'red'; // Set the background color
+  }, []);
+
   return (
     <main className={styles.main}>
       <div className={styles.description}>
