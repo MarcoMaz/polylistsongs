4ab52f9e7045cd9ddc2ccd20bd2a1a1f79c781db
Marco Mazzai <mazzai.marco@gmail.com>
2023-10-28 22:49:48 +0200

    ( app ): clean up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,11 @@
 "use client";
-import React, { useEffect } from 'react';
-import styles from './page.module.css'
+import React from 'react';
 
 export default function Home() {
-  useEffect(() => {
-    const params = new URLSearchParams(window.location.search);
-    const backgroundColor = params.get('backgroundColor');
-
-    document.body.style.backgroundColor = backgroundColor || 'red'; // Set the background color
-  }, []);
 
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-       HELLO THIS IS A TEST
-      </div>
+    <main>
+      Code here
     </main>
   )
 }
