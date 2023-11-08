e0cd1e3690105ab38b3aba88a366434c31303a5c
Marco Mazzai <mazzai.marco@gmail.com>
2023-11-08 10:57:46 +0100

    - add drummers selection

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,16 +1,50 @@
 /* eslint-disable @next/next/no-img-element */
 "use client";
 
+import { useState } from "react";
 import { songsData } from "../../../pages/api/songs";
 
 const Search = () => {
+  const drummers: string[] = songsData.map((song) => song.drummer);
+
+  const [selectedDrummers, setSelectedDrummers] = useState<string[]>([]);
+
+  const handleDrummerSelection = (drummer: string) => {
+    if (selectedDrummers.includes(drummer)) {
+      setSelectedDrummers(selectedDrummers.filter((item) => item !== drummer));
+    } else {
+      setSelectedDrummers([...selectedDrummers, drummer]);
+    }
+  };
+
+  const filteredSongsData =
+    selectedDrummers.length === 0
+      ? songsData
+      : songsData.filter((song) => selectedDrummers.includes(song.drummer));
+
   return (
     <div className="container">
       <header>Im header</header>
-      <aside>Im aside</aside>
+      <aside>
+        <strong>Drummers</strong>
+        {drummers.map((drummer, index) => {
+          return (
+            <div key={index}>
+              <input
+                type="checkbox"
+                id={`drummer${index}`}
+                name={`drummer${index}`}
+                checked={selectedDrummers.includes(drummer)}
+                onChange={() => handleDrummerSelection(drummer)}
+              />
+              <label htmlFor={`drummer${index}`}>{drummer}</label>
+            </div>
+          );
+        })}
+      </aside>
       <main>
         <ul>
-          {songsData.map(
+          {filteredSongsData.map(
             (
               {
                 title,
@@ -31,13 +65,14 @@ const Search = () => {
                 <li key={index}>
                   <details>
                     <summary>
-                      {title} by {artist} - {polyrhythm.against}:{polyrhythm.base}
+                      {title} by {artist} - {polyrhythm.against}:
+                      {polyrhythm.base}
                     </summary>
                     {title} by {artist}, from <em>{album}</em> ({year})<br />
                     drummer: {drummer} - type: {polyType} <br />
                     <br />
-                    {polyrhythm.against}:{polyrhythm.base} starting at {timestamp}{" "}
-                    <br />
+                    {polyrhythm.against}:{polyrhythm.base} starting at{" "}
+                    {timestamp} <br />
                     <br />
                     Time signature: {timeSignature.numerator} -{" "}
                     {timeSignature.denominator} <br />
@@ -45,7 +80,12 @@ const Search = () => {
                     {scoreUrl && (
                       <>
                         score{" "}
-                        <img src={scoreUrl} alt="image" width={20} height={20} />{" "}
+                        <img
+                          src={scoreUrl}
+                          alt="image"
+                          width={20}
+                          height={20}
+                        />{" "}
                         <br />
                         <br />
                       </>
@@ -63,17 +103,3 @@ const Search = () => {
 };
 
 export default Search;
-
-/*
-title,
-  album,
-  artist,
-  drummer,
-  polyType,
-  year,
-  timestamp,
-  polyrhythm,
-  timeSignature,
-  source,
-  scoreUrl
-  */
